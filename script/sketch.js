ef67a7e55b511b9ddffd4f0e86bf2e3fcd99a2db
SeoHanaa <126849505+SeoHanaa@users.noreply.github.com>
2023-12-18 15:13:28 +0900

    참고링크업로드

--- script/sketch.js
+++ script/sketch.js
@@ -1,4 +1,27 @@
 /**
+ * // P_2_3_1_02
+ * 참고한 작품의 주소
+ * https://editor.p5js.org/generative-design/sketches/P_2_3_1_02
+//
+// Generative Gestaltung – Creative Coding im Web
+// ISBN: 978-3-87439-902-9, First Edition, Hermann Schmidt, Mainz, 2018
+// Benedikt Groß, Hartmut Bohnacker, Julia Laub, Claudius Lazzeroni
+// with contributions by Joey Lee and Niels Poldervaart
+// Copyright 2018
+//
+// http://www.generative-gestaltung.de
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+ */
+
+/**
  * 조작법
 
  * mouse click         : 시작
@@ -55,7 +78,7 @@ function draw() {
 //랜덤한 길이
 function mousePressed() {
   lineSize = random(120, 200);
-  img;
+  // img;
 }
 
 function keyReleased() {
